d149c167d5e69f3b7fdca0d864b932be4504e2d3
yljios <634320873@qq.com>
2018-04-10 14:49:40 +0800

    folder change name App

--- Applications/App/start_gateway.php
+++ Applications/App/start_gateway.php
@@ -18,23 +18,23 @@ use \GatewayWorker\BusinessWorker;
 use \Workerman\Autoloader;
 
 // gateway 进程，这里使用Text协议，可以用telnet测试
-$gateway = new Gateway("tcp://0.0.0.0:8282");
+$gateway = new Gateway("Websocket://0.0.0.0:8282");
 // gateway名称，status方便查看
-$gateway->name = 'YourAppGateway';
+$gateway->name = 'WebIMGateway';
 // gateway进程数
 $gateway->count = 4;
 // 本机ip，分布式部署时使用内网ip
 $gateway->lanIp = '127.0.0.1';
 // 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
 // 则一般会使用4000 4001 4002 4003 4个端口作为内部通讯端口 
-$gateway->startPort = 2900;
+$gateway->startPort = 5000;
 // 服务注册地址
 $gateway->registerAddress = '127.0.0.1:1238';
 
 // 心跳间隔
-//$gateway->pingInterval = 10;
+$gateway->pingInterval = 20;
 // 心跳数据
-//$gateway->pingData = '{"type":"ping"}';
+$gateway->pingData = '{"type":"ping"}';
 
 /* 
 // 当客户端连接上来时，设置连接的onWebSocketConnect，即在websocket握手时的回调
